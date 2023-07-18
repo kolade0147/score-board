f2934fbd1b8cff750e4d0683b98d735c79f66d1a
kolade0147 <boluwatifekolade94@gmail.com>
2023-07-18 16:32:51 +0100

    score.board

---
+++ index.js
@@ -0,0 +1,31 @@
+let countKy = document.getElementById("countKyOne")
+let countKy2 = document.getElementById("countKyTwo")
+
+let countKay = 0
+function hscores1(){
+countKay++
+countKy.textContent = countKay
+}
+function hscores2(){
+countKay += 2
+countKy.textContent = countKay
+}
+function hscores3(){
+countKay += 3
+countKy.textContent = countKay
+}
+
+let countKay2 = 0
+function gscores1() {
+countKay2++
+countKy2.textContent = countKay2
+}
+function gscores2() {
+countKay2 += 2
+countKy2.textContent = countKay2
+}
+function gscores3() {
+countKay2 += 3
+countKy2.textContent = countKay2
+}
+
